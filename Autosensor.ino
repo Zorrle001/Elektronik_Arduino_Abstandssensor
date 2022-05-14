194bdd6b85d1ef8f801991b6d423400653c0e8d2
Zorrle001 <77202203+Zorrle001@users.noreply.github.com>
2022-05-15 00:44:08 +0200

    Update Autosensor.ino

--- Autosensor.ino
+++ Autosensor.ino
@@ -1,3 +1,4 @@
+// REGISTRIERUNG ALLER PINS UND BENÖTIGTEN VARIABLEN
 int trigger = 7;
 int echo = 6;
 int piezo = 5;
@@ -6,8 +7,10 @@ long entfernung = 0;
 long puls = 0;
 
 void setup() {  
-
-  Serial.begin(9600);
+  
+  Serial.begin(9600); //VERBINDUNGSAUFBAU MIT AUSGABE AN PC
+	
+  //INITIALISIERUNG DER INPUT & OUTPUT PINS
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(piezo, OUTPUT);
@@ -15,7 +18,9 @@ void setup() {
 }
 
 void loop() {
-	
+
+  //BERECHNUNG DES ABSTANDES ZU NÄCHSTEM OBJEKT
+  //für weitere Details siehe Readme.md
   digitalWrite(trigger, LOW);
   delay(5);
   digitalWrite(trigger, HIGH);
@@ -25,27 +30,35 @@ void loop() {
   dauer = pulseIn(echo, HIGH);
   entfernung = (dauer/2) * 0.03432;
   
+  //ÜBERPRÜFUNG OB KEIN OBJEKT GEFUNDEN WURDE
   if(entfernung == 1203) {
 	  
-    Serial.println("Abgebrochen!");
-    digitalWrite(piezo, LOW);
+    Serial.println("Abgebrochen!"); 	//AUSGABE WIRD AN PC GESENDET
+    digitalWrite(piezo, LOW);		//LAUTSPRECHER WIRD DEAKTIVIERT, FALLS ER NOCH AKTIVIERT IST
     return;
 	
   }else {
-	  
-    Serial.print(entfernung);
+	 
+    //ENTFERNUNG ZU NÄCHSTEM OBJEKT WIRD AN PC GESENDET
+    Serial.print(entfernung);		
     Serial.println(" cm");
 
+    //ÜBERPRÜFEN OB OBJEKT IN WARNBEREICH LIEGT
     if(entfernung <= 23 && entfernung >= 0) {
-		
+      //WENN OBJEKT IN BEREICH LIEGT WIRD DER TEIL AUSGEFÜHRT:
+	    
+      //BERECHNEN DES ZEITABSTANDES IN PIEPSGERÄUSCH
       puls = (31 * entfernung - 218) / 2; 
 	  
+      //ENTFERNEN VON NEGATIVEN WERTEN
       if(puls < 0) {
         puls = 0;
       }
-	  
+	
+      //ÜBERPRÜFEN OB ZEITABSTAND GRÖßERGLEICH 30 IST
       if(puls >= 30) {
 		  
+	//WENN ZEITABSTAND ÜBER 30MS IST WIRD PIEPS SIGNAL AUSGEGEBEN
         digitalWrite(piezo, HIGH);
         delay(puls);
         digitalWrite(piezo, LOW);
@@ -53,13 +66,16 @@ void loop() {
         return;
 		
       }else {
+	//ANSONSTEN GIBT LAUTSPRECHER DAUERTON AUS
         digitalWrite(piezo, HIGH);
       }
 	  
     }else {
+      //ANSONSTEN WIRD DER LAUTSPRECHER DEAKTIVIERT, FALLS ER NOCH AKTIVIERT IST
       digitalWrite(piezo, LOW);
     }
   }
   
+  //NUN WIRD 500MS GEWARTET BIS ALLES VON VORNE BEGINNT
   delay(500);
 }
